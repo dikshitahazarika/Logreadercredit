38fb3e79c16f2d6923bc580e3e1178411812c5f3
dikshitahazarika <33933386+dikshitahazarika@users.noreply.github.com>
2019-10-18 15:03:20 +0100

    LogEntry

---
+++ src/main/java/codetest/LogEntry.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codetest;
+
+/**
+ *
+ * @author Dikshita
+ */
+public class LogEntry {
+
+	private String id;
+	private String state;
+	private long timestamp;
+	private String type;
+	private String host;
+	
+	
+	public LogEntry(String id, String state, long timestamp, String type, String host) {
+		this.setId(id);
+		this.setState(state);
+		this.timestamp = timestamp;
+		this.type = type;
+		this.host = host;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
+
